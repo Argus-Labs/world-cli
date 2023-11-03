ac823c77de32b753571d72f658973c3ead3e3d48
Brian Yeh <brian@argus.gg>
2023-11-03 16:46:30 -0700

    only sigterm if process still exists.

--- cmd/cardinal/dev.go
+++ cmd/cardinal/dev.go
@@ -2,13 +2,14 @@ package cardinal
 
 import (
 	"fmt"
-	"github.com/magefile/mage/sh"
-	"github.com/spf13/cobra"
 	"os"
 	"os/exec"
 	"os/signal"
-	"pkg.world.dev/world-cli/tea/style"
 	"syscall"
+
+	"github.com/magefile/mage/sh"
+	"github.com/spf13/cobra"
+	"pkg.world.dev/world-cli/tea/style"
 )
 
 const (
@@ -46,7 +47,7 @@ var devCmd = &cobra.Command{
 		}
 
 		// Run Cardinal
-		execCmd, err := runCardinal()
+		cardinalExecCmd, err := runCardinal()
 		if err != nil {
 			return err
 		}
@@ -59,7 +60,7 @@ var devCmd = &cobra.Command{
 		cmdErr := make(chan error, 1)
 
 		go func() {
-			err := execCmd.Wait()
+			err := cardinalExecCmd.Wait()
 			cmdErr <- err
 		}()
 
@@ -71,9 +72,11 @@ var devCmd = &cobra.Command{
 				return errCleanup
 			}
 
-			err = execCmd.Process.Signal(syscall.SIGTERM)
-			if err != nil {
-				return err
+			if cardinalExecCmd.ProcessState == nil && cardinalExecCmd.Process != nil {
+				err = cardinalExecCmd.Process.Signal(syscall.SIGTERM)
+				if err != nil {
+					return err
+				}
 			}
 
 			return nil
