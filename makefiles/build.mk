fc8844bd652e3489b4268258654ecbe771761ce4
heronimus <adie@heronimus.id>
2024-03-22 23:45:09 +0700

    fix build workflow

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -3,7 +3,7 @@ goreleaser_version=v1.24.0
 
 goreleaser-install:
 	@echo "--> Checking if goreleaser $(goreleaser_version) is installed"
-	@if [ $$(goreleaser --version 2> /dev/null | grep GitVersion | awk '{ print $$2 }') != "$(goreleaser_version)" ]; then \
+	@if [ "$$(goreleaser --version 2> /dev/null | grep GitVersion | awk '{ print $$2 }')" != "$(goreleaser_version)" ]; then \
 		echo "--> Installing goreleaser $(goreleaser_version)"; \
 		go install github.com/goreleaser/goreleaser@$(goreleaser_version); \
 	else \
