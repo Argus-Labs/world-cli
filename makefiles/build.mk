d5cfb24d96c6a0f8318100fbbef0edcc1cecea75
heronimus <adie@heronimus.id>
2024-03-22 23:34:08 +0700

    chore: update make build, add goreleaser-install function

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -1,4 +1,16 @@
+goreleaser_version=v1.24.0
+
+goreleaser-install:
+	@echo "--> Checking if goreleaser $(goreleaser_version) is installed"
+	@if [ $$(goreleaser --version | grep GitVersion | awk '{ print $$2 }') != "$(goreleaser_version)" ]; then \
+		echo "--> Installing golangci-lint $(goreleaser_version)"; \
+		go install github.com/goreleaser/goreleaser@$(goreleaser_version); \
+	else \
+		echo "--> goreleaser $(goreleaser_version) is already installed"; \
+	fi
+
 build:
-	go build -o $(PKGNAME) -v ./cmd/$(PKGNAME)
-	
+	@$(MAKE) goreleaser-install
+	goreleaser build --clean --snapshot
+
 .PHONY: build
