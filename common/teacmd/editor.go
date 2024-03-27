324c92b089246e6b49c98da11cc7c5c4b13d1343
Ryan Martin <rmrt1n@users.noreply.github.com>
2024-03-28 03:41:17 +0700

    feat: replace project ids with guids

--- common/teacmd/editor.go
+++ common/teacmd/editor.go
@@ -7,12 +7,18 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 const (
-	latestReleaseURL = "https://api.github.com/repos/Argus-Labs/cardinal-editor/releases/latest"
-	httpTimeout      = 5 * time.Second
+	latestReleaseURL             = "https://api.github.com/repos/Argus-Labs/cardinal-editor/releases/latest"
+	httpTimeout                  = 5 * time.Second
+	tmpZipFileName               = "tmp.zip"
+	cardinalProjectIDPlaceholder = "__CARDINAL_PROJECT_ID__"
 )
 
 type Asset struct {
@@ -47,15 +53,19 @@ func SetupCardinalEditor() error {
 		return err
 	}
 
-	err = downloadAndUnzipFle(release.Assets[0].BrowserDownloadURL)
+	err = downloadRelease(release.Assets[0].BrowserDownloadURL)
 	if err != nil {
+		return err
+	}
+
+	if err = unzipFile(); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func downloadAndUnzipFle(url string) error {
+func downloadRelease(url string) error {
 	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
 	if err != nil {
 		return err
@@ -70,7 +80,13 @@ func downloadAndUnzipFle(url string) error {
 	}
 	defer resp.Body.Close()
 
-	err = unzipFile(resp.Body)
+	file, err := os.Create(tmpZipFileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
 	if err != nil {
 		return err
 	}
@@ -78,20 +94,8 @@ func downloadAndUnzipFle(url string) error {
 	return nil
 }
 
-func unzipFile(src io.Reader) error {
-	tmp := "tmp.zip"
-	file, err := os.Create(tmp)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, src)
-	if err != nil {
-		return err
-	}
-
-	reader, err := zip.OpenReader(tmp)
+func unzipFile() error {
+	reader, err := zip.OpenReader(tmpZipFileName)
 	if err != nil {
 		return err
 	}
@@ -134,9 +138,46 @@ func unzipFile(src io.Reader) error {
 		return err
 	}
 
-	if err = os.Remove(tmp); err != nil {
+	if err = os.Remove(tmpZipFileName); err != nil {
+		return err
+	}
+
+	if err = replaceProjectIDs(); err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func replaceProjectIDs() error {
+	assetsDir := ".editor/assets"
+	files, err := os.ReadDir(assetsDir)
+	if err != nil {
+		return err
+	}
+
+	// "ce" prefix is added because guids can start with numbers, which is not allowed in js
+	projectID := "ce" + strippedGUID()
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), ".js") {
+			content, err := os.ReadFile(filepath.Join(assetsDir, file.Name()))
+			if err != nil {
+				return err
+			}
+
+			newContent := strings.ReplaceAll(string(content), cardinalProjectIDPlaceholder, projectID)
+
+			err = os.WriteFile(filepath.Join(assetsDir, file.Name()), []byte(newContent), 0600)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func strippedGUID() string {
+	u := uuid.New()
+	return strings.ReplaceAll(u.String(), "-", "")
 }
