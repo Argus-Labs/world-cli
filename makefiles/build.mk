07b05458d2aab86ec0860a3150df98deb4309d01
Heronimus Adie <hero@heronimus.id>
2024-03-29 03:51:17 +0700

    ci: use GoReleaser for build & release binary (#49)

--- makefiles/build.mk
+++ makefiles/build.mk
@@ -1,4 +1,44 @@
+SHELL := /bin/bash
+goreleaser_version=v1.24.0
+
+goreleaser-install:
+	@echo "--> Checking if goreleaser $(goreleaser_version) is installed"
+	@if [ "$$(goreleaser --version 2> /dev/null | grep GitVersion | awk '{ print $$2 }')" != "$(goreleaser_version)" ]; then \
+		echo "--> Installing goreleaser $(goreleaser_version)"; \
+		go install github.com/goreleaser/goreleaser@$(goreleaser_version); \
+	else \
+		echo "--> goreleaser $(goreleaser_version) is already installed"; \
+	fi
+
 build:
-	go build -o $(PKGNAME) -v ./cmd/$(PKGNAME)
-	
-.PHONY: build
+	@$(MAKE) goreleaser-install
+	@goreleaser build --clean --snapshot
+	@echo "--> Build binary is available in the ./dist directory"
+
+release:
+	$(eval args_count := $(words $(MAKECMDGOALS)))
+	$(eval args_release_tag := $(word 2, $(MAKECMDGOALS)))
+	@if [ "$(args_count)" != "2" ]; then \
+		echo " [Error] Wrong argument!";\
+		echo -e " --> usage: make release <tag-version>\n"; \
+		exit 1; \
+	fi
+	@if [ -z "${GITHUB_TOKEN}" ]; then\
+		echo " [Error] GITHUB_TOKEN env variable not found!"; \
+		echo " --> GoReleaser requires an API token with the repo scope to deploy the artifacts to GitHub."; \
+		echo -e "     (https://github.com/settings/tokens/new)\n"; \
+		exit 1; \
+	fi
+	@echo "--> Release Tag: $(args_release_tag)"
+	@echo "--> git: tags current commit"
+	@git tag -a $(args_release_tag) -m "goreleaser: $(args_release_tag)"
+	@echo "--> git: push tag $(args_release_tag)"
+	@git push origin $(args_release_tag)
+	@echo "--> goreleaser release"
+	goreleaser release --clean
+
+## do-nothing targets for extra unused args passed into @release
+%:
+	@:
+
+.PHONY: build goreleaser-install release
--- makefiles/dev.mk
+++ makefiles/dev.mk
@@ -2,22 +2,17 @@ clean:
 	@echo "--> Cleaning up"
 	@echo "--> Running go clean"
 	@go clean
-	@echo "--> Removing binary"
-	@rm -f $(PKGNAME)
+	@echo "--> Removing build './dist' directory"
+	@rm -rf ./dist
 	@echo "--> Removing coverage files"
 	@find . -type f -name "*.out" -exec rm -f {} \;
 
 
 install:
 	@echo "--> Installing World CLI"
-	@echo "--> Building binary"
-	@go build -o $(PKGNAME) -v ./cmd/$(PKGNAME)
-	@echo "--> Copying binary to /usr/local/bin"
 	@mkdir -p /usr/local/bin
-	@cp $(PKGNAME) /usr/local/bin
-	@chmod +x /usr/local/bin/$(PKGNAME)
+	@echo "--> Building binary, install to /usr/local/bin"
+	@goreleaser build --clean --single-target --snapshot -o /usr/local/bin/$(PKGNAME)
 	@echo "--> Installed $(PKGNAME) to /usr/local/bin"
-	@echo "--> Cleaning up"
-	@rm -f $(PKGNAME)
-	
+
 .PHONY: clean install
