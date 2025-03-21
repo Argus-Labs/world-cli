53e92d4206de978bde50b18820dd784b622d23ff
Ed Zavada <edmund@argus.gg>
2025-03-21 14:16:13 -0400

    Misc fixes, especially install related
    - default app version shouldn't include "v" prefix
    - fixed NewTeaProgram() to pass nil for input
    - fixed deprecated format directives in goreleaser yaml
    - use NewTeaProgram somewhere it was breaking before as a test.

--- cmd/world/forge/common.go
+++ cmd/world/forge/common.go
@@ -250,7 +250,7 @@ func slugCheck(slug string, minLength int, maxLength int) error {
 // if you are not on a TTY, so you can run the debugger. Call it just as you would call tea.NewProgram().
 func NewTeaProgram(model tea.Model, opts ...tea.ProgramOption) *tea.Program {
 	if !term.IsTerminal(int(os.Stderr.Fd())) {
-		opts = append(opts, tea.WithInput(os.Stdin))
+		opts = append(opts, tea.WithInput(nil))
 		// opts = append(opts, tea.WithoutRenderer())
 	}
 	return tea.NewProgram(model, opts...)
