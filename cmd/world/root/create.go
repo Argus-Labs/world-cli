c72e198f06c2507959e7b1146104633fc94b7a67
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-04-23 23:10:52 +0300

    Update create.go

--- cmd/world/root/create.go
+++ cmd/world/root/create.go
@@ -34,7 +34,7 @@ func NewLogCmd(log string) tea.Cmd {
 // Bubble Tea Model //
 //////////////////////
 
-type WorldCreateModel struct { //nolint:decorder
+type WorldCreateModel struct { //nolint:decoder
 	logs             []string
 	steps            steps.Model
 	projectNameInput textinput.Model
