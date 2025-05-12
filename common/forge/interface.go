25bbdd6de411a1f25a18903eed34ea77a69e6c1b
Ed Zavada <ed.zavada@gmail.com>
2025-05-12 14:26:06 -0400

    Update common/forge/interface.go
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- common/forge/interface.go
+++ common/forge/interface.go
@@ -4,7 +4,7 @@ import "context"
 
 // Forge defines the interface for common forge operations.
 type Forge interface {
-	// login will perform the login process for the user, including organization & project creation/selection.
+	// Login will perform the login process for the user, including organization & project creation/selection.
 	Login(ctx context.Context) error
 }
 
